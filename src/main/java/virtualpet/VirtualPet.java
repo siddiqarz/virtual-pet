7d1e2028750fb889330654bfff72784bda7931fa
Siddiqa <siddiqarasheed@gmail.com>
2018-05-20 16:32:05 -0400

    end of class update

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -1,48 +1,49 @@
 package virtualpet;
 
 public class VirtualPet {
-	// long startTime = System.nanoTime();
-	// long estimatedTime = System.nanoTime()-startTime;
+	 
 	private String petName = "";
-	public int hunger;
-	public int sleepiness;
-	public int boredom;
-	public int age;
-	public int health;
+	private int hunger;
+	private int sleepiness;
+	private int boredom;
+	private int age;
+	private int health;
 
-	public VirtualPet(String petName) {
+	public VirtualPet( String petName,int hunger, int sleepiness, int boredom, int age, int health) {
 		this.petName = petName;
-		System.out.println(petName + " the Dragon");
-	}
-
-	public void petStats(int hunger, int sleepiness, int boredom, int age, int health) {
 		this.hunger = hunger;
 		this.sleepiness = sleepiness;
-		this.boredom = boredom;
-		this.age = age;
-		this.health = health;
+		System.out.println("Hi! I'm " + petName + " the Dragon!");
 		System.out.println("Your pet's stats are: ");
-		System.out.println("hunger: " + hunger + "\nsleepiness " + sleepiness + "\nboredom " + boredom + "\nage " + age
-				+ "\nhealth " + health);
+		System.out.println("Hunger: " + hunger + "\nSleepiness " + sleepiness + "\nBoredom " + boredom + "\nAge " + age
+				+ "\nHealth " + health);
 	}
-public void hunger(int foodType) {
-	System.out.println("What would you like to give " + petName);
-	System.out.println("1. Apple\n2. Sheep\n3. Grass\n4. Fish\n5. Rice ");
-}
-	public int feed(int nutritionValue) {
-		 hunger = hunger + nutritionValue;
-		 if (nutritionValue<0) {
-			 System.out.println("Disgusting!");
+	
+	public int getHunger() {
+		return hunger;
+	}
+
+	public void feed(int nutritionValue) {
+		hunger = hunger + (nutritionValue);
+		
+		 if (nutritionValue==-1) {
+			 System.out.println("Disgusting!!!");
 		 }
-		 else if (nutritionValue<5) {
-			 System.out.println("Thanks for the meal. I guess.");
+		 else if (nutritionValue==-5) {
+			 System.out.println("Yumm!!!");
+			 age = age + 1;
 		 }
 		 else if(nutritionValue == 0) {
 			 System.out.println("Whatever.");
+			 health = health -1;
 			 
 		 }
-		 else System.out.println("Yuum!!!");
-		 return hunger;
+		 else System.out.println("Thanks for the meal I guess");
+		 
+		if (hunger <0) {
+				System.out.println("I can't eat no more!!");
+				hunger = 0;
+			}
 //		health = health + nutritionValue-sleepiness;
 	}
 
@@ -52,6 +53,31 @@ public void hunger(int foodType) {
 		System.out.println("2. Sing " + petName + " a lullaby");
 		System.out.println("3. Play with " + petName);
 		System.out.println("4. Do nothing");
+		System.out.println("5. Check my stats");
+	}
+
+	public int tick() {
+			hunger = hunger +2;
+			boredom = boredom +1;
+			sleepiness = sleepiness + 1;
+			return hunger;
+		
+	}
+
+	public int getBoredom() {
+		return boredom;
+	}
+
+	public int getSleepiness() {
+		return sleepiness;
+			}
+
+	public int getAge() {
+		return age;
+	}
+
+	public int getHealth() {
+		return health;
 	}
 
 }
--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -6,40 +6,73 @@ import java.util.Scanner;
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-	
+
 		Scanner input = new Scanner(System.in);
-		//ask user to name their pet 
-		System.out.println("Please name your Dragon: ");
-		//Store pet name
+		// ask user to name their pet
+		System.out.println("Choose a name for your Dragon: ");
+		// Store pet name
 		String petName = input.nextLine();
-		VirtualPet dragon = new VirtualPet(petName);
-		
-		//create random numbers to assign to initialize pet stats
+
+		// create random numbers to assign to initialize pet stats
 		Random rand = new Random();
-		int hunger = rand.nextInt(10) +1;
-		int sleepiness = rand.nextInt(10) +1;
-		int boredom = rand.nextInt(10) +1;
+		int hunger = rand.nextInt(5) + 1;
+		int sleepiness = rand.nextInt(10) + 1;
+		int boredom = rand.nextInt(10) + 1;
 		int age = 0;
 		int health = 10;
 		
-		//tick starts here?
-		dragon.petStats(hunger, sleepiness, boredom, age, health);
-		dragon.action();
-		int newAction = input.nextInt();
-		int foodValue;
-		if (newAction == 1) {
-			dragon.hunger(newAction);
-			int foodType = input.nextInt();
+		//call out initial stats
+		VirtualPet dragon = new VirtualPet(petName, hunger, sleepiness, boredom, age, health);
+		
+		
+		//loops while pet is alive
+		while (hunger != 10 && health != 0) {
+			dragon.action();
+			int newAction = input.nextInt();
+			int nutritionValue;
+			int foodType = 0;
+			
+			if (newAction == 1) {
+				System.out.println("What would you like to give " + petName);
+				System.out.println("1. Apple\n2. Sheep\n3. Grass\n4. Fish\n5. Rice\n6. Candy ");
+				foodType = input.nextInt();
 			switch (foodType) {
-			case 1: foodValue = -1;
-			case 2: foodValue = 5;
-			case 3: foodValue = -5;
-			case 4: foodValue = 3;
-			case 5: foodValue = 1;
-			default: foodValue = 0;
+			case 1:
+				nutritionValue = -1;
+				break;
+			case 2:
+				nutritionValue = -5;
+				break;
+			case 3:
+				nutritionValue = -1;
+				break;
+			case 4:
+				nutritionValue = -3;
+				break;
+			default:
+				nutritionValue = 0;
 			}
-			dragon.feed(foodValue);
-			dragon.petStats(hunger, sleepiness, boredom, age, health);
+			dragon.feed(nutritionValue);
+			System.out.println("My hunger level is now: " + dragon.getHunger());
+			}
+			if(newAction == 4) {
+				if(dragon.getHunger()>5) {
+			System.out.println("I'm hungry");
+				}
+				else if (dragon.getBoredom()>5) {
+					System.out.println("So boooring, my buddy!");
+				}
+				else if (dragon.getSleepiness()>8) {
+					System.out.println("*Yawn!*, I can't keep my eyes open.");
+				}
+				else System.out.println("*Takes off and burns everything to a crisp*");
+			}
+		dragon.tick();
+		if(newAction == 5) {
+			System.out.println("Updated Pet Stats: " + "\nHunger: " + dragon.getHunger() + "\nSleepiness: " + 
+					dragon.getSleepiness()+ "\nBoredom: " + dragon.getBoredom() + "\nAge: " + dragon.getAge() + "\nHealth: " + dragon.getHealth() );
+		}
+		
 		}
 	}
 
